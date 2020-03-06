7d58ce868f1da9bf67e1a903438c7489744a3a47
Faris Alotaibi <faa339@nyu.edu>
2020-03-06 11:43:56 -0500

    adding pipe handler

--- shell.c
+++ shell.c
@@ -16,22 +16,30 @@ Description: An interactive shell.
 #define MAX_ARGS 1024
 
 void ErrorHandle(void);
-void freeArgv(char** argv);
-void cdExecute(char** argv, char* workindirect);
-int tokenProcess(char** argv, char* argstring);
-void argExec(char ** argv, int argc);
 void argStringFormat(char* argstring);
+int tokenProcess(char** argv, char* argstring);
+int pipeHandle(char** argv, int argc);
+void cdExecute(char** argv, char* workindirect);
+void argExec(char** argv, int argc);
+
 
 int main()
 {
 	errno = 0;
+	//Setting up all string variables here to avoid unnecessary 
+	//reallocation
 	char argstring[PATH_MAX];
-	char *token;
+	char *token = NULL;
 	char workindirect[PATH_MAX];
+	char **argv = (char**) malloc(MAX_ARGS * sizeof(char*));
+	char* prompt = NULL;
+
 	while(1)
 	{
 		int argc = 0;
-		char* prompt = NULL;
+		//Have to clear argv here, dont want it all messy from previous cmnds
+		argv = memset(argv, 0, MAX_ARGS);
+
 		if((prompt != NULL) || ((prompt = getenv("PS1")) != NULL))
 			printf("%s: ",prompt);
 		else
@@ -40,19 +48,20 @@ int main()
 			printf("%s: ",workindirect);
 		}
 		
-		char **argv = (char**) malloc(MAX_ARGS * sizeof(char*));
-		
 		if((fgets(argstring, PATH_MAX, stdin)) == NULL) ErrorHandle();
 		argStringFormat(argstring);
 
-		if((strcmp(argstring, "exit")) == 0)
-			exit(EXIT_SUCCESS);
-		
 		argc = tokenProcess(argv, argstring);
+
+		//Block to handle built-in usage possibilities
 		if(strcmp(argv[0], "cd") == 0 && argc < 3)
 			cdExecute(argv, workindirect);
 		else if(strcmp(argv[0], "cd") == 0 && argc>= 3)
 			printf("cd: too many arguments\n");
+		else if((strcmp(argv[0], "exit")) == 0 && (argc < 3))
+			exit(EXIT_SUCCESS);
+		else if((strcmp(argv[0], "exit")) == 0 && (argc >= 3))
+			printf("exit: too many arguments\n");
 		else
 			argExec(argv, argc);
 	}
@@ -60,14 +69,15 @@ int main()
 
 void ErrorHandle(void)
 {
+	//Display the error message
+	//Since we want to stay in the shell, we don't exit(EXIT_FAILURE)
 	if(errno)
-	{
 		perror("Error");
-	}
 }
 
 void argStringFormat(char* argstring)
 {
+	//Put a null character at the first \n seen (end of input for us)
 	for(int i=0; i<PATH_MAX; i++)
 	{
 		if(argstring[i] == '\n')
@@ -80,6 +90,8 @@ void argStringFormat(char* argstring)
 
 int tokenProcess(char** argv, char* argstring)
 {
+	//Turn argstring into a series of tokens and place it into argv
+	//Then return an argc 
 	int argcount = 0;
 	char* token = strtok(argstring, " ");
 	while(token  != NULL)
@@ -90,9 +102,34 @@ int tokenProcess(char** argv, char* argstring)
 	return argcount;
 }
 
+int pipeHandle(char** argv, int argc)
+{
+	for(int i=0; i< argc; i++)
+	{
+		if(strmp(argv[i], "<") == 0)
+		{
+			return 0
+		}else if(strcmp(argv[i], ">") == 0)
+		{
+			return 0
+		}else if(argv[i][strlen(argv[i])-1] == '>')
+		{
+			return 0
+		}else if(argv[i][0] == '<')
+		{
+			return 0
+		}else if(strcmp(argv[i], ">>") == 0)
+		{
+
+		}else return -1;
+	}
+}
+
 void cdExecute(char** argv, char* workindirect)
 {
-	int chtest;
+	//Func to handle changing directories & changing our environment string
+	//to reflect the change
+	int chtest = 0;
 	if (argv[1])
 		chtest = chdir(argv[1]);
 	else
@@ -105,13 +142,15 @@ void cdExecute(char** argv, char* workindirect)
 
 void argExec(char** argv, int argc)
 {
-	int childpid = fork(), waitstat = 0;
-	if(childpid == 0)
+	//Execute any cmnds that arent exit or cd here 
+	int pid = fork(), waitstat = 0;
+	if(pid == 0)
 	{
 		execvp(argv[0], argv);
 		if(errno) ErrorHandle();
 	}else
 	{
-		wait(&waitstat);
+		wait(&waitstat); 
+		//All good parents wait for their children!
 	}
 }
