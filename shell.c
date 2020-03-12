660d56b3ac6ceb034600b3fb90c7e358fa8cff1f
Faris Alotaibi <faa339@nyu.edu>
2020-03-11 23:22:06 -0400

    Final Version

--- shell.c
+++ shell.c
@@ -25,8 +25,8 @@ void getPrompt(char* prompt);
 void argStringFormat(char* argstring);
 int tokenProcess(char** argv, char* argstring);
 int redirHandle(char* redirpath, int redirOp);
-void builtInCheck(char ** argv, char* workindirect, int argc);
-void cdExecute(char** argv, char* workindirect);
+int builtInCheck(char ** argv, int argc);
+void cdExecute(char** argv);
 int createEargs(char** execargs, char** argv, int argc);
 void argExec(char** argv, int argc);
 void signalSetup(__sighandler_t handler);
@@ -36,16 +36,15 @@ int main()
 	errno = 0;
 	//Setting up all string variables here to avoid unnecessary reallocs
 	char argstring[PATH_MAX];
-	char *token = NULL;
 	char workindirect[PATH_MAX];
-	char **argv = (char**) malloc(MAX_ARGS * sizeof(char*));
+	char **argv = (char**) malloc((MAX_ARGS * sizeof(char*))+1);
 	if (argv == NULL) 
 	{
-		printf("Malloc failure\n");
+		fprintf(stderr,"Malloc failure\n");
 		exit(EXIT_FAILURE);
 	}
 	int argc;
-	char* prompt = NULL;
+	char* userprompt;
 	//We want to ignore SIGINTS and SIGQUITS while in our shells main process
 	signalSetup(SIG_IGN);
 	while(1)
@@ -55,8 +54,8 @@ int main()
 		//Have to clear argv here, dont want it all messy from previous cmnds
 
 		//Check if we were given a custom prompt string
-		if((prompt != NULL) ||((prompt = getenv("PS1")) != NULL))
-			printf("%s: ",prompt);
+		if((userprompt != NULL) ||((userprompt = getenv("PS1")) != NULL))
+			printf("%s: ",userprompt);
 		else
 		{ 
 			if(getcwd(workindirect,PATH_MAX-1) == NULL)
@@ -85,7 +84,8 @@ int main()
 			continue;
 		}
 		//Check if we were given cd or exit, else execute the arg
-		builtInCheck(argv, workindirect, argc);
+		if(builtInCheck(argv, argc) == 1)
+			argExec(argv, argc);
 	}
 } 
 
@@ -93,8 +93,11 @@ void errorHandle(void)
 {
 	//Display the error message
 	//Since we want to stay in the shell, we don't exit(EXIT_FAILURE)
-	if(errno)
+	if(errno != 0)
+	{
 		perror("Error");
+		errno = 0;
+	}	
 }
 
 void argStringFormat(char* argstring)
@@ -125,18 +128,31 @@ int tokenProcess(char** argv, char* argstring)
 	return argcount;
 }
 
-void builtInCheck(char** argv, char* workindirect, int argc)
+int builtInCheck(char** argv, int argc)
 {
+	//Execute cd or exec
+	//Return 1 if the command wasnt cd or exec
 	if((strcmp(argv[0], "cd") == 0) && argc < 3)
-		cdExecute(argv, workindirect);
+	{
+		cdExecute(argv);
+		return 0;
+	}
 	else if((strcmp(argv[0], "cd") == 0) && argc>= 3)
+	{	
 		fprintf(stderr,"cd: Too many arguments\n");
+		return 0;
+	}
 	else if((strcmp(argv[0], "exit") == 0) && (argc < 3))
+	{
 		exit(EXIT_SUCCESS);
+	}
 	else if((strcmp(argv[0], "exit") == 0) && (argc >= 3))
+	{
 		fprintf(stderr,"exit: Too many arguments\n");
+		return 0;
+	}
 	else
-		argExec(argv, argc);
+		return 1;
 }
 
 void signalSetup(__sighandler_t handler)
@@ -144,7 +160,7 @@ void signalSetup(__sighandler_t handler)
 	//Set up signals in here based on value of handler
 	struct sigaction new_action;
 	new_action.sa_handler = handler;
-
+	
 	if((sigaction(SIGINT, &new_action,NULL) < 0))
 	{
 		errorHandle();
@@ -197,7 +213,7 @@ int redirHandle(char* redirpath, int redirOp)
 	}else return -1;
 }
 
-void cdExecute(char** argv, char* workindirect)
+void cdExecute(char** argv)
 {
 	//Func to handle changing directories & changing our environment string
 	int chtest = 0;
@@ -207,8 +223,6 @@ void cdExecute(char** argv, char* workindirect)
 		chtest = chdir(".");
 
 	if(chtest<0) errorHandle();
-
-	if ((getcwd(workindirect,PATH_MAX-1)) == NULL) errorHandle();
 }
 
 int createEargs(char** execargs, char** argv, int argc)
@@ -218,6 +232,7 @@ int createEargs(char** execargs, char** argv, int argc)
 	int eargs=1, redirstat=0;
 	execargs[0] = argv[0];
 	//Iterate over argv looking for any redirection characters
+	//Characters included at < > 2> >>
 	for(int i=1; i<argc; i++)
 	{
 		if(((strcmp(argv[i], "<") == 0) && argv[i+1]))
@@ -264,21 +279,28 @@ int createEargs(char** execargs, char** argv, int argc)
 void argExec(char** argv, int argc)
 {
 	//Execute any cmnds that arent exit or cd here
-	int execount=0, waitstat=0, exectest=0;
+	int execount=0, waitstat=0, exectest = 0;
 	int pid = fork();
 	if(pid == 0)
 	{
 		//Change our signals back to default in child for early exits
 		signalSetup(SIG_DFL);
-		char** execargs = (char**) malloc(argc*sizeof(char*));
+		char** execargs = (char**) malloc((argc * sizeof(char*))+1);
+		if(execargs < 0)
+		{
+			errorHandle();
+			exit(EXIT_FAILURE);
+		}
 		execargs = memset(execargs, 0 , argc * sizeof(char*));
 		execount = createEargs(execargs, argv, argc);
 		//Manually null terminating our string to be safe
 		execargs[execount] = NULL; 
 		exectest = execvp(execargs[0], execargs);
 		if(exectest < 0) 
+		{
 			errorHandle();
-		exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
+		}
 	}else
 	{
 		wait(&waitstat); 
